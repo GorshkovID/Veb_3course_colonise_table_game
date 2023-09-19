a6ed45f5541079a9bbe45d6b4441a1c3b789584e
Игорь Горшков <gorshkovigor00@gmail.com>
2023-09-19 15:33:39 +0500

    1

---
+++ scripts/modal.js
@@ -0,0 +1,25 @@
+﻿// Открыть модальное окно
+document.getElementById("open-rules-btn").addEventListener("click", function () {
+    document.getElementById("rules").classList.add("open")
+})
+
+// Закрыть модальное окно
+document.getElementById("close-rules-modal-btn").addEventListener("click", function () {
+    document.getElementById("rules").classList.remove("open")
+})
+
+// Закрыть модальное окно с помощью Esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("rules").classList.remove("open")
+    }
+})
+
+// Закрыть модальное окно при клике вне его
+document.querySelector("#rules .rules_content").addEventListener('click', event => {
+    event._isClickWidthInModal = true;
+});
+document.getElementById("rules").addEventListener('click', event => {
+    if (event._isClickWidthInModal) return;
+    event.currentTarget.classList.remove('open');
+})
